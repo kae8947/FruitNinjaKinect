8afe4bcd4007c68e82d5190b00341284de31dc0c
kae8947 <kae89477@gmail.com>
2016-02-22 22:58:27 -0500

    Initial commit

---
+++ Assets/3D Models/Accessories/Yodas Lightsaber/Readme.txt
@@ -0,0 +1,13 @@
+---------------TheAkamu---------------
+
+Hope you like my Model of Yodas Lightsaber
+
+You need the file "51" for the dark part´s relief of the grip
+
+Hope you understand what I wanted to express ;) Sorry for my bad English
+
+Best wishes from Germany!!!
+
+TheAkamu
+
+--------------------------------------
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Player;
+    private Vector3 offset;
+
+
+
+	void Start () {
+        offset = transform.position - Player.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = Player.transform.position + offset;
+	}
+}
+
