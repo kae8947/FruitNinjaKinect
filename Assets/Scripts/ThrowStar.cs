e7226700d18cb7a0f1c98542713aa938eeb44213
jessiemi <qiuwen.yu@Knights.ucf.edu>
2016-03-10 22:37:52 -0500

    Throwing Ninja (Press mouse down).

---
+++ Assets/Scripts/ThrowStar.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ThrowStar : MonoBehaviour {
+
+	public Rigidbody starPrefab;
+	public float Star_forward_Force = 1000;
+	public float distanceToCheck = 100f;
+
+
+	void Update () 
+	{
+		
+
+			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			//GameObject Star = (GameObject)Instantiate (star_prefab, transform.position, Quaternion.LookRotation (pos));
+
+			if(Input.GetButtonDown("Fire1"))
+			{
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				
+
+				if(Physics.Raycast(ray, out hit, distanceToCheck)){
+				
+					Rigidbody starInstance;
+					starInstance = Instantiate(starPrefab, transform.position, Quaternion.LookRotation (ray.direction)) as Rigidbody;
+					starInstance.AddForce(transform.forward * Star_forward_Force);
+
+				}
+			}
+
+	}
+
+
+}
---
+++ Assets/Scripts/starAttacks.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class starAttacks : MonoBehaviour {
+
+	public int scoreValue = 10;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Fruit")) {
+
+			other.gameObject.GetComponent<ExplodingFruit>().Explode(); 	
+			Destroy (gameObject, 2f);
+			Destroy (other.gameObject, 3f);
+
+			ScoreManager.score += scoreValue;
+		}
+	}
+}
